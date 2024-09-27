81cbee799224e20ddf7ca650e65e6eeebf5a47ef
Mike Sokolov <miha13.sokolov@gmail.com>
2024-09-27 19:25:17 +0300

    setup finished

--- lua/config/clangd.lua
+++ lua/config/clangd.lua
@@ -1,54 +1,28 @@
-local on_attach = function(client, bufnr)
-    vim.g.completion_matching_strategy_list = "['exact', 'substring', 'fuzzy']"
+-- local on_attach = function(client, bufnr)
+-- 	vim.g.completion_matching_strategy_list = "['exact', 'substring', 'fuzzy']"
+--
+-- 	local function buf_set_option(...)
+-- 		vim.api.nvim_buf_set_option(bufnr, ...)
+-- 	end
+--
+-- 	-- Enable completion triggered by <c-x><c-o>
+-- 	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+--
+-- end
 
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-    local function buf_set_option(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
-
-    -- Enable completion triggered by <c-x><c-o>
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- Mappings.
-    local opts = { noremap=true, silent=true }
-
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<leader>,', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-    buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-end
-
-
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-require('lspconfig').clangd.setup{
-    capabilities = capabilities,
-    on_attach = on_attach,
-}
+require("lspconfig").clangd.setup({
+	capabilities = capabilities,
+	-- on_attach = on_attach,
+})
 local function setup_clangd()
-    require('lspconfig').clangd.setup({
-        capabilities = capabilities,
-        on_attach = on_attach
-    })
+	require("lspconfig").clangd.setup({
+		capabilities = capabilities,
+		-- on_attach = on_attach,
+	})
 end
 
 return {
-    setup_clangd = setup_clangd
+	setup_clangd = setup_clangd,
 }
